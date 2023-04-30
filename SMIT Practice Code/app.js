0d90ad108fe81f7504020864447e7e2d63bf0388
JoharAbbas-313 <123437515+JoharAbbas-313@users.noreply.github.com>
2023-04-30 04:19:17 -0700

    Add files via upload

---
+++ SMIT Practice Code/app.js
@@ -0,0 +1,125 @@
+// function first(){
+//   alert("Hello World");
+// }
+// first();
+// first();
+// first();
+// first();
+// first();
+// first();
+// first();
+
+
+// function first(b,a){
+// return b + a;
+// }
+// alert(first(2,3));
+// alert(first(2,3));
+// alert(first(2,3));
+
+
+//  function first(b,a){
+//  return b + a;
+//  }
+// console.log(first(2,3));
+// console.log(first(2,3));
+// console.log(first(2,3));
+
+// reference error
+
+// function first(a){
+// var a = "johar";
+// }
+// console.log(first(a));
+
+// var a = "johar ";
+// function foo(){
+// b = "Abbas";
+// }
+// foo();
+// console.log(a);
+
+
+// var a = "johar ";
+// function foo(){
+// b = "Abbas";
+// }
+// foo();
+// console.log(a + b);
+
+//  Nan error
+
+// function foo(a,b){
+// return a + b;
+// }
+// console.log(foo(1));
+
+
+// function naan(a,c){
+//   return a - c;
+// }
+
+// console.log(naan(4));
+
+
+// function foo(a,j = 3){
+// return a + j;
+// }
+// console.log(foo(2,));
+
+
+// function foo(a,b = 3){
+// return a+b;
+// }
+
+// console.log(foo(3,4));
+
+// Undefine error
+
+// function foo(){
+// return ;
+// }
+// console.log(foo());
+
+// nan error
+
+//  function foo(a,b){
+//  return a+b;
+//  }
+//  console.log(foo());
+
+
+// switch format 
+  
+
+// var nam = "johar";
+
+// switch  (nam) {
+//   case "johar":
+//   alert("Hello johar")
+//   break;
+
+//   case "abbas":
+//   alert("Hello abbas")
+//   break;
+
+//   default:
+//     alert("Hello koyi nhi")
+
+// }
+
+// var names = "rahat";
+
+// switch (names){
+
+//   case "rahat":
+//     alert("Hello rahat");
+//     break
+
+//     case "ali":
+//       alert("Hello ali");
+// break
+
+//       default:
+//         alert("hello nobady");
+//   }
